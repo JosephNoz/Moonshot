7d0a9b0826c58e976aa9568087705ee0e206b588
ricnoz <70532922+ricnoz@users.noreply.github.com>
2023-08-21 12:12:26 +0200

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Joseph Norris on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
